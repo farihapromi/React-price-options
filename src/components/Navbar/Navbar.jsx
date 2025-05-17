fc8aaa8e8806f671745a6472cf08cf4ee338915f
Fariha Promi <farihapromi2611@gmail.com>
2025-05-17 11:20:15 +0600

    Add daisyUi for Nav

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  const routes = [
+  { path: '/', id: 1, name: 'Home' },
+  { path: '/about', id: 2, name: 'About' },
+  { path: '/dashboard', id: 3, name: 'Dashboard' },
+  { path: '/login', id: 4, name: 'Login' },
+  { path: '/settings', id: 5, name: 'Settings' }
+];
+
+  return (
+    <nav>
+        
+    </nav>
+  )
+}
+
+export default Navbar
